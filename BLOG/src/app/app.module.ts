48f12046b6c8186d233053f19df07632a4811af1
randorsg <randorsamuel@hotmail.com>
2021-05-24 08:20:11 +0200

    creacion formulario componente

--- BLOG/src/app/app.module.ts
+++ BLOG/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BlogComponent } from './blog/blog.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BlogComponent
+    BlogComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
